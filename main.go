4db2c541b5d214a9850be37866d04c634fb1e4fe
Paul Tagliamonte <tag@pault.ag>
2014-12-04 23:07:05 -0500

    Add in some API key love

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"./openstates"
+	"fmt"
+)
+
+func main() {
+	leg := openstates.GetLegislator("COL000036")
+	fmt.Printf(leg.FirstName + "\n")
+	fmt.Printf(leg.LastName + "\n")
+}
--- sunlight/sunlight.go
+++ sunlight/sunlight.go
@@ -1,8 +1,32 @@
 package sunlight
 
-var (
-	apiKey string
+import (
+	"io/ioutil"
+	"os"
+	"os/user"
 )
+
+var (
+	apiKey string = getDefaultAPIKey()
+)
+
+func getDefaultAPIKey() string {
+	ekey := os.Getenv("SUNLIGHT_API_KEY")
+	if ekey != "" {
+		return ekey
+	}
+
+	usr, _ := user.Current()
+	dir := usr.HomeDir
+
+	fkey, err := ioutil.ReadFile(dir + "/.sunlight.key")
+
+	if err != nil {
+		return ""
+	}
+
+	return string(fkey)
+}
 
 func GetAPIKey() string {
 	return apiKey
