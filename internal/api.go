37bf4ba73ae03d62c6794ae296d4bd13f2ffeaf3
Paul Tagliamonte <tag@pault.ag>
2014-12-04 22:29:55 -0500

    Update OpenStates

--- internal/api.go
+++ internal/api.go
@@ -1,6 +1,9 @@
 package internal
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 	"net/url"
 	"strings"
 )
@@ -21,6 +24,19 @@ func QueryURL(root string, params map[string]string, resource ...string) string 
 		uri = uri + "&" + url.QueryEscape(k) + "=" + url.QueryEscape(v)
 	}
 	return uri
+}
+
+func GetURL(target interface{}, root string, params map[string]string, resource ...string) {
+	url := QueryURL(root, params, resource...)
+	res, err := http.Get(url)
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	json.Unmarshal(body, target)
 }
 
 func GetAPIKey() string {
--- openstates/openstates.go
+++ openstates/openstates.go
@@ -4,6 +4,15 @@ import (
 	"../internal"
 )
 
-func Legislators(map[string]string) {
-	internal.GenerateURL("", "")
+var openstatesRoot string = "http://openstates.org/api/v1/"
+
+type Legislator struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+func GetLegislator(bigId string) *Legislator {
+	l := new(Legislator)
+	internal.GetURL(l, openstatesRoot, map[string]string{}, "legislators", bigId)
+	return l
 }
