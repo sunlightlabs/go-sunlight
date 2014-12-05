7926cc0b13b9e338a857331edabcbd3be746875d
Paul Tagliamonte <tag@pault.ag>
2014-12-04 23:12:24 -0500

    Add more to the dict

--- main.go
+++ main.go
@@ -9,4 +9,5 @@ func main() {
 	leg := openstates.GetLegislator("COL000036")
 	fmt.Printf(leg.FirstName + "\n")
 	fmt.Printf(leg.LastName + "\n")
+	fmt.Printf(leg.PhotoUrl + "\n")
 }
--- openstates/openstates.go
+++ openstates/openstates.go
@@ -6,9 +6,21 @@ import (
 
 var openstatesRoot string = "http://openstates.org/api/v1/"
 
+type Source struct {
+	Url string
+}
+
 type Legislator struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
+	FirstName  string   `json:"first_name"`
+	LastName   string   `json:"last_name"`
+	FullName   string   `json:"full_name"`
+	Sources    []Source `json:"sources"`
+	Id         string   `json:"id"`
+	MiddleName string   `json:"middle_name"`
+	State      string   `json:"state"`
+	Active     bool     `json:"active"`
+	PhotoUrl   string   `json:"photo_url"`
+	Url        string   `json:"url"`
 }
 
 func GetLegislator(bigId string) *Legislator {
