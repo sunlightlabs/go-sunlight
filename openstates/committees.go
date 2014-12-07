b000e60c6294e9830944c8ff1b04a739c212c2a2
Paul Tagliamonte <tag@pault.ag>
2014-12-07 15:36:13 -0500

    Add committees

---
+++ openstates/committees.go
@@ -0,0 +1,41 @@
+package openstates
+
+import (
+	"github.com/sunlightlabs/go-sunlight/internal"
+)
+
+type CommitteeMember struct {
+	LegId string `json:"leg_id"`
+	Name  string `json:"name"`
+	Role  string `json:"role"`
+}
+
+type Committee struct {
+	AllIds       []string          `json:"all_ids"`
+	Chamber      string            `json:"chamber"`
+	Committee    string            `json:"committee"`
+	Id           string            `json:"id"`
+	ParentId     string            `json:"parent_id"`
+	State        string            `json:"state"`
+	Subcommittee string            `json:"subcommittee"`
+	Members      []CommitteeMember `json:"members"`
+	Sources      []Source          `json:"sources"`
+}
+
+func GetCommittee(bigId string) (*Committee, error) {
+	t := &Committee{}
+	err := internal.GetURL(t, openstatesRoot, map[string]string{}, "committees", bigId)
+	if err != nil {
+		return nil, err
+	}
+	return t, nil
+}
+
+func GetCommittees(criteria map[string]string) (*[]Committee, error) {
+	t := &[]Committee{}
+	err := internal.GetURL(t, openstatesRoot, criteria, "committees")
+	if err != nil {
+		return nil, err
+	}
+	return t, nil
+}
